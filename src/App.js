84defe84563a426788d297c56574dfeb87876eab
unknown <brian_warner25@yahoo.com>
2018-09-03 14:34:51 -0700

    state form

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title"
-import friends from "./friends.json";
-import "./App.css";
+import React from "react";
+import Form from "./components/Form";
 
-class App extends Component {
-  state = {
-    friends
-  };
+const App = () => <Form />;
 
-  removeFriend = id => {
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    this.setState({ friends });
-  };
-
-  render(){
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-          removeFriend={this.removeFriend}
-          id={friend.id}
-          key={friend.id}
-          name={friend.name}
-          image={friend.image}
-          occupation={friend.occupation}
-          location={friend.location}
-          />
-        ))}
-      </Wrapper>
-
-    );
-  }
-}
-
-export default App
-
-
+export default App;
