0cd306e4fb9c37eb0aa3f16e3921b7e6bef8e3e0
unknown <brian_warner25@yahoo.com>
2018-09-03 14:14:34 -0700

    friend class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title"
+import friends from "./friends.json";
+import "./App.css";
 
-const App = () => <Counter />;
+class App extends Component {
+  state = {
+    friends
+  };
 
-export default App;
+  removeFriend = id => {
+    const friends = this.state.friends.filter(friend => friend.id !== id);
+    this.setState({ friends });
+  };
+
+  render(){
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+          removeFriend={this.removeFriend}
+          id={friend.id}
+          key={friend.id}
+          name={friend.name}
+          image={friend.image}
+          occupation={friend.occupation}
+          location={friend.location}
+          />
+        ))}
+      </Wrapper>
+
+    );
+  }
+}
+
+export default App
+
+
